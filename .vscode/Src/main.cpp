7b7a427689b0d0c23505b7f79ed09932562de4a1
AliceAuto <sgq2742798794@gmail.com>
2024-11-02 22:06:56 +0800

    oj

--- .vscode/Src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "system.h"
-using namespace std;
-
-int main() {
-    cout << "Hello, world!" << endl;
-    cout<<"This is a test program."<<endl;
-    return 0;
-}
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # 物流系统
  物流系统
 
+# 后端开发项目文件
+物流系统\Src\CppDll\CppDll.sln
+# 前端开发项目文件
+物流系统\Src\PyScript\PyScript.sln
 
 
 
-**本项目采取多文件构建，以便合作编写**
