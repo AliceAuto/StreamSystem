9f022d6707ef6d5b30b69cda0045c12deb60b61b
AliceAuto <sgq2742798794@gmail.com>
2024-11-05 17:39:41 +0800

    更新 main.py

--- 物流系统/Src/PyScript/main.py
+++ 物流系统/Src/PyScript/main.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
     app = Win(controller)  # 将控制器传递给主窗口                   controller ->UI
     controller.init(app)  # 初始化控制器绑定UI                      UI -> controller
     app.mainloop()  # 启动应用程序
+    
+
+
+
+
 
