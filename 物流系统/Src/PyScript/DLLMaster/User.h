bfdd81b9e41fa9503f79324b0a24837ca90f2b12
AliceAuto <sgq2742798794@gmail.com>
2024-11-24 17:52:18 +0800

    d

--- 物流系统/Src/PyScript/DLLMaster/User.h
+++ 物流系统/Src/PyScript/DLLMaster/User.h
@@ -3,7 +3,16 @@
 #include <string>
 using namespace std;
 class User {
+public:
 	size_t id;
 	string name;
 	vector<size_t> shop_history;//¹ºÂòÀúÊ·
+	//==================================
+	void print() {
+		
+	}
+	
 };
+
+
+
