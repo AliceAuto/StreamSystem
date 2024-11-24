955ecfad846b0a37403ab7bc3daaa6c73c02d275
AliceAuto <sgq2742798794@gmail.com>
2024-11-24 17:56:05 +0800

    更新 printFormat.h

--- 物流系统/Src/PyScript/DLLMaster/printFormat.h
+++ 物流系统/Src/PyScript/DLLMaster/printFormat.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "Object.h"
+
+
+template<typename T> 
+void print(const std::vector<T> &vec) {
+	cout << "======================================" << endl;
+	for (auto e : vec)cout << e << endl;
+	cout << "======================================" << endl;
+}
