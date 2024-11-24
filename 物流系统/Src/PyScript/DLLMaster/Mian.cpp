74dc553bcf8546049ec241f30c7b92404c711274
AliceAuto <sgq2742798794@gmail.com>
2024-11-24 17:59:16 +0800

    e

--- 物流系统/Src/PyScript/DLLMaster/Mian.cpp
+++ 物流系统/Src/PyScript/DLLMaster/Mian.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+
+#include "printFormat.h"
+
+
+
+
 
 using namespace std;
 int main() {
-
+	print<int>({ 2,32,32,3,23,2 });
 	cout << "HHHHHHHHHHHHHH" << endl;
 	return 0;
 }
+
+
--- 物流系统/Src/PyScript/DLLMaster/printFormat.h
+++ 物流系统/Src/PyScript/DLLMaster/printFormat.h
@@ -4,7 +4,7 @@
 
 template<typename T> 
 void print(const std::vector<T> &vec) {
-	cout << "======================================" << endl;
-	for (auto e : vec)cout << e << endl;
-	cout << "======================================" << endl;
+	std::cout << "======================================" << std::endl;
+	for (auto e : vec)std::cout << e << std::endl;
+	std::cout << "======================================" << std::endl;
 }
