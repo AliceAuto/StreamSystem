4890acc647c6939caddf3d67dc8cbea5d6ed4a8b
cheats1314 <3030240693@qq.com>
2024-11-25 10:41:55 +0800

    Update Mian.cpp

--- 物流系统/Src/PyScript/DLLMaster/Mian.cpp
+++ 物流系统/Src/PyScript/DLLMaster/Mian.cpp
@@ -10,15 +10,7 @@
 
 using namespace std;
 int main() {
-<<<<<<< HEAD
-	cout << "HHHHHHHHHHHHHH" << endl;
+
+	cout << "NEW" << endl;
 	return 0;
 }
-=======
-	print<int>({ 2,32,32,3,23,2 });
-	cout << "HHHHHHHHHHHHHH" << endl;
-	return 0;
-}
-
-
->>>>>>> 74dc553bcf8546049ec241f30c7b92404c711274
